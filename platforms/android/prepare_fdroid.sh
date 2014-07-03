9a96a2c8ec6c3fab26f99a10ec0cc35ff3000c12
GreenAddress.it <info@greenaddress.it>
2014-07-03 06:51:35 +0200

    Fixed building on x86_64, added mips and x86 for scrypt

--- platforms/android/prepare_fdroid.sh
+++ platforms/android/prepare_fdroid.sh
@@ -11,7 +11,19 @@ cp -r platform_www/* assets/www
 cp -r ../../plugins/me.apla.cordova.app-preferences/www/task assets/www
 
 cd scrypt
-export PATH=$ANDROID_NDK/toolchains/arm-linux-androideabi-4.8/prebuilt/linux-x86/bin:$PATH
-make NDK_ROOT=$ANDROID_NDK TARGET=android
+patch -p1 < ../scrypt_Makefile.patch
+if [ `uname -m` = "x86_64" ]; then
+    export SYSTEM="linux-x86_64"
+else
+    export SYSTEM="linux-x86"
+fi
+for ARCH in arm-linux-androideabi mipsel-linux-android x86; do
+    export ARCH_SHORT=`echo $ARCH | cut -d'-' -f 1 | sed s/mipsel/mips/` # arm|mips|x86
+    export PATH=$ANDROID_NDK/toolchains/$ARCH-4.8/prebuilt/$SYSTEM/bin:$PATH
+    make clean TARGET=android
+    make NDK_ROOT=$ANDROID_NDK TARGET=android ARCH_SHORT=$ARCH_SHORT ARCH=`echo $ARCH | sed s/x86/i686-linux-android/`
+    ARCHDIR=../libs/`echo $ARCH_SHORT | sed s/arm/armeabi/` # armeabi|mips|x86
+    mkdir -p $ARCHDIR
+    cp target/libscrypt.so $ARCHDIR
+done
 cd ..
-cp scrypt/target/libscrypt.so libs/armeabi
