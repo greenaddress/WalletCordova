294cbdc1cefc8d2dcae9bf1d585a848cac5754aa
BTChip <contact@btchip.com>
2014-10-18 23:56:12 +0200

    HW.1 HID transport

--- platforms/android/src/com/btchip/comm/android/BTChipTransportAndroid.java
+++ platforms/android/src/com/btchip/comm/android/BTChipTransportAndroid.java
@@ -31,64 +31,20 @@ import android.hardware.usb.UsbManager;
 import com.btchip.BTChipException;
 import com.btchip.comm.BTChipTransport;
 
-public class BTChipTransportAndroid implements BTChipTransport {
-	
-	private UsbDeviceConnection connection;
-	private UsbInterface dongleInterface;
-	private UsbEndpoint in;
-	private UsbEndpoint out;
-	private int timeout;
-	private byte transferBuffer[];
-	
-	BTChipTransportAndroid(UsbDeviceConnection connection, UsbInterface dongleInterface, UsbEndpoint in, UsbEndpoint out, int timeout) {
-		this.connection = connection;
-		this.dongleInterface = dongleInterface;
-		this.in = in;
-		this.out = out;
-		this.timeout = timeout;
-		transferBuffer = new byte[260];
-	}
-
-	@Override
-	public byte[] exchange(byte[] command) throws BTChipException {
-		int result = connection.bulkTransfer(out, command, command.length, timeout);
-		if (result != command.length) {
-			throw new BTChipException("Write failed");
-		}
-		result = connection.bulkTransfer(in, transferBuffer, transferBuffer.length, timeout);
-		if (result < 0) {
-			throw new BTChipException("Write failed");
-		}
-		int sw1 = (int)(transferBuffer[0] & 0xff);
-		int sw2 = (int)(transferBuffer[1] & 0xff);
-		if (sw1 != SW1_DATA_AVAILABLE) {
-			byte[] response = new byte[2];
-			response[0] = (byte)sw1;
-			response[1] = (byte)sw2;
-			return response;
-		}
-		byte[] response = new byte[sw2 + 2];
-		System.arraycopy(transferBuffer, 2, response, 0, sw2 + 2);
-		return response;
-	}
-
-	@Override
-	public void close() throws BTChipException {
-		connection.releaseInterface(dongleInterface);
-		connection.close();
-	}
-	
+public class BTChipTransportAndroid {
+		
 	public static UsbDevice getDevice(UsbManager manager) {
 		HashMap<String, UsbDevice> deviceList = manager.getDeviceList();
 		for (UsbDevice device : deviceList.values()) {
-			if ((device.getVendorId() == VID) && (device.getProductId() == PID)) {
+			if ((device.getVendorId() == VID) && 
+			   ((device.getProductId() == PID_WINUSB) || (device.getProductId() == PID_HID))) {
 				return device;
 			}
 		}
 		return null;		
 	}
 	
-	public static BTChipTransportAndroid open(UsbManager manager, UsbDevice device) {
+	public static BTChipTransport open(UsbManager manager, UsbDevice device) {
 		// Must only be called once permission is granted (see http://developer.android.com/reference/android/hardware/usb/UsbManager.html)
 		// Important if enumerating, rather than being awaken by the intent notification
 		UsbInterface dongleInterface = device.getInterface(0);
@@ -105,11 +61,18 @@ public class BTChipTransportAndroid implements BTChipTransport {
         }
         UsbDeviceConnection connection = manager.openDevice(device);
         connection.claimInterface(dongleInterface, true);
-        return new BTChipTransportAndroid(connection, dongleInterface, in, out, TIMEOUT);        
+        if (device.getProductId() == PID_WINUSB) {
+        	return new BTChipTransportAndroidWinUSB(connection, dongleInterface, in, out, TIMEOUT);
+        }
+        else {
+        	return new BTChipTransportAndroidHID(connection, dongleInterface, in, out, TIMEOUT);
+        }
 	}
 	
-	private static final int SW1_DATA_AVAILABLE = 0x61;
+	public static final String LOG_STRING = "BTChip";
+	
 	private static final int VID = 0x2581;
-	private static final int PID = 0x1b7c;
+	private static final int PID_WINUSB = 0x1b7c;
+	private static final int PID_HID = 0x2b7c;
 	private static final int TIMEOUT = 20000;	
 }
