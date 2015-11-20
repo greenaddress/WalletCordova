9179029ff0b95f0cc098d97e2835c4cc3e280671
GreenAddress <info@greenaddress.it>
2015-11-20 16:39:28 +0100

    Fixed activity name and facebook plugin

--- plugins/it.greenaddress.cordova/scripts/removeMainActivity.js
+++ plugins/it.greenaddress.cordova/scripts/removeMainActivity.js
@@ -2,9 +2,21 @@ var fs = require('fs');
 var path = require('path');
 
 module.exports = function(context) {
-    fs.unlink(path.join(
-    	context.opts.projectRoot,
-    	'platforms', 'android', 'src', 'it', 'greenaddress', 'cordova',
-    	'MainActivity.java'  // is overridden by GreenAddress.java
+    var activity_path = path.join(
+        context.opts.projectRoot,
+        'platforms', 'android', 'src', 'it', 'greenaddress', 'cordova',
+        'MainActivity.java'  // is overridden by GreenAddressIt.java
+    );
+    if (fs.existsSync(activity_path)) {
+        // can be already removed in after_plugin_install
+        fs.unlink(activity_path);
+    }
+
+    var manifest_path = path.join(
+        'platforms', 'android', 'AndroidManifest.xml'
+    );
+    var contents = fs.readFileSync(manifest_path, {encoding: 'utf-8'});
+    fs.writeFileSync(manifest_path, contents.replace(
+        'MainActivity', 'GreenAddressIt'
     ));
 }
