d5fecf01dee9c69ddddd9efd6475099a20aab38a
GreenAddress <info@greenaddress.it>
2016-02-05 12:11:53 +0000

    Fee bumping without 2FA within user defined limits

--- www/greenaddress.it/static/js/greenwallet/transactions/controllers.js
+++ www/greenaddress.it/static/js/greenwallet/transactions/controllers.js
@@ -163,11 +163,17 @@ angular.module('greenWalletTransactionsControllers',
                 return tx_sender.call(
                     'http://greenaddressit.com/vault/send_raw_tx',
                     builder.build().toHex(), twofac_data
-                );
+                ).then(function(data) {
+                    if (data.limit_decrease) {
+                        $scope.wallet.limits.total -= data.limit_decrease;
+                    }
+                });
             }
             // try without 2FA to see if it's required
             // (could be bump amount under the user-defined 2FA threshold)
-            return try_sending().catch(function(e) {
+            return try_sending({
+                'try_under_limits_bump': feeDelta
+            }).catch(function(e) {
                 if (e.args && e.args[0] &&
                         e.args[0] == "http://greenaddressit.com/error#auth") {
                     return wallets.get_two_factor_code(
