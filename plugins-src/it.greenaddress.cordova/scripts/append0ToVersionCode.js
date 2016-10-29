de9c5475e2d6fdb3861c8ef0f94066e95458dfde
Jerzy Kozera <jerzy.kozera@gmail.com>
2016-10-30 01:32:54 +0200

    Append 0 to version code (https://issues.apache.org/jira/browse/CB-8976)

---
+++ plugins-src/it.greenaddress.cordova/scripts/append0ToVersionCode.js
@@ -0,0 +1,17 @@
+var path = require('path'),
+    fs = require('fs');
+
+module.exports = function(context) {
+    var et = context.requireCordovaModule('elementtree'),
+        manifestPath = path.join(context.opts.projectRoot, 'platforms', 'android', 'AndroidManifest.xml'),
+        manifestContents = fs.readFileSync(manifestPath, { encoding: 'utf8' }),
+        manifest = et.parse(manifestContents),
+        versionCode = manifest.getroot().get('android:versionCode') + '',
+        buildExtrasPath = path.join(context.opts.projectRoot, 'platforms', 'android', 'build-extras.gradle');
+
+    if (context.opts.cordova.plugins.indexOf('cordova-plugin-crosswalk-webview') === -1) {
+        versionCode += '0'
+    }
+    var data = fs.readFileSync(buildExtrasPath)
+    fs.writeFileSync(buildExtrasPath, data+"\next.cdvVersionCode=" + versionCode, { encoding: 'utf8' });
+};
