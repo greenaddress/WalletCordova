f0a8acc12838e4cda2e6a988ad319b4036069225
Goncalo Carvalho <goncalo@blockstream.io>
2017-10-10 11:48:19 +0200

    fix plugman version

--- prepare.sh
+++ prepare.sh
@@ -119,6 +119,9 @@ fi
 # Add the wally plugin:
 if [ \! -e libwally-core ]; then
     git clone https://github.com/ElementsProject/libwally-core -b master --depth 1
+    cd libwally-core
+    patch -p1 < ../wally.patch
+    cd ..
 fi
 # Build the wally plugin
 ./prepare_wally.sh
